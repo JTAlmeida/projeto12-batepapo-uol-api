3adb17099eae57803f6f956c18032b51d378d1e3
Jonatas <jonatasalmeidasm@gmail.com>
2022-09-01 11:20:16 -0300

    post and get to '/participants' route implemented and tested (with validations, but without joi), database linked and working as expected

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import dayjs from "dayjs";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
@@ -11,14 +12,63 @@ app.use(cors());
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 
 let db;
-mongoClient.connect().then(() => {
-  db = mongoClient.db("batepapo-uol");
+
+mongoClient
+  .connect()
+  .then(() => {
+    db = mongoClient.db("batepapo-uol");
+  })
+  .catch((err) => {
+    console.log(`Error ${err} while trying to connect to database`);
+  });
+
+app.get("/participants", async (req, res) => {
+  try {
+    const data = await db.collection("participants").find().toArray();
+    return res.send(data);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
 });
 
-app.get("/participants", (req, res)=>{
-    db.collection("participants").find().toArray().then(data => {
-        res.send(data);
-    })
+app.post("/participants", async (req, res) => {
+  const { name } = req.body;
+
+  try {
+    if (!name) {
+      return res.status(422).send({ message: "Please insert a name." });
+    }
+
+    const checkName = await db
+      .collection("participants")
+      .findOne({ name: name });
+
+    if (checkName && checkName.name === name) {
+      return res.status(429).send({
+        message: "Name already being used, pleased insert a different one.",
+      });
+    }
+
+    await db
+      .collection("participants")
+      .insertOne({ name, lastStatus: Date.now() });
+
+    const newParticipantMessage = {
+      from: name,
+      to: "Todos",
+      text: "entra na sala...",
+      type: "status",
+      time: dayjs().format("hh:mm:ss"),
+    };
+
+    await db.collection("messages").insertOne(newParticipantMessage);
+
+    return res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
 });
 
 app.listen(5000, () => {
